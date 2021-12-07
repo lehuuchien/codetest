a61f7c0a0705e322c7fa8126cc4ae51a47066522
ChienLe <chienle@tdt.asia>
2021-12-07 08:53:05 +0700

    Initial Commit

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Le Huu Chien on 07/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
